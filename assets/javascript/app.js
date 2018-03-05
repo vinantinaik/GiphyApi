a4756103b5bf573a01d7b5422c64ef75728e90ef
Vinanti <vinantinaik@gmail.com>
2018-03-04 21:50:38 -0600

    added some styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var countries = ["USA", "India", "United Kingdom", "Mexico", "Iran", "Australlia", "Brazil"];
+var countries = ["USA", "India", "United Kingdom", "France", "Japan", "Cuba", "Brazil"];
 var appKey = "Y61qWuspMl6ayaDlpV7ZsyxdU4ACWIzo";
 
 $(document).ready(function () {
@@ -6,63 +6,77 @@ $(document).ready(function () {
     renderButtons();
 
     function renderButtons() {
+
+        $("#country-view").empty();
         for (var i = 0; i < countries.length; i++) {
-            var btn = $("<button>").addClass("btn").attr("data-name", countries[i]).text(countries[i]);
-
-
+            var btn = $("<button>").addClass("btn btn-info").attr("data-name", countries[i]).text(countries[i]);
             $("#country-view").append(btn);
+        }
 
+
+    }
+    $(document).on("click", ".btn-info", displayCountry);
+    $(document).on("click", ".img", previewImage);
+
+    $("#add-country").click(function (event) {
+        event.preventDefault();
+        var country = $("#country-input").val().trim();
+        var countryExists = countries.indexOf(country);
+        if (countryExists === -1)
+        {
+            countries.push(country);
+            renderButtons();
 
         }
 
-       
-    }
-    $(document).on("click", ".btn", displayCountry);
-    $(document).on("click",".img",previewImage);
 
-    function previewImage(){
+
+    })
+
+    function previewImage() {
 
         var displayOriginal = $(this).attr("data-displayOriginal");
 
-        if(displayOriginal === "true")
-        {
-            $(this).attr("src",$(this).attr("data-preview"));
-            $(this).attr("data-displayOriginal",false);
+        if (displayOriginal === "true") {
+            $(this).attr("src", $(this).attr("data-preview"));
+            $(this).attr("data-displayOriginal", false);
 
         }
-        else
-        {
-            $(this).attr("src",$(this).attr("data-original"));
-            $(this).attr("data-displayOriginal",true);
+        else {
+            $(this).attr("src", $(this).attr("data-original"));
+            $(this).attr("data-displayOriginal", true);
 
         }
 
-       
+
     }
 
     function displayCountry() {
-
-      
-
         var country = $(this).attr("data-name");
 
-       
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + appKey + "&q=" + country + "&limit=5&offset=0&rating=G&lang=en";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + appKey + "&q=" + country + "&limit=12&offset=0&lang=en";
 
         $.ajax({
             method: "GET",
             url: queryURL,
         }).then(function (response) {
-           
+
             $("#country-image").empty();
             $.each(response.data, function (i, value) {
 
+
+                var divTag =$("<div>").addClass("col -md-4");
+
+                var h1Tag=$("<h5>").text("Rating: " + value.rating);
                 var imgTag = $("<img>").addClass("img").attr({
                     "src": value.images.original_still.url, "data-preview":
-                    value.images.preview_gif.url,"data-original":value.images.original_still.url,"data-displayOriginal":true
+                        value.images.preview_gif.url, "data-original": value.images.original_still.url, "data-displayOriginal": true
                 });
-                $("#country-image").append(imgTag);
-               
+
+                divTag.append(h1Tag).append(imgTag);
+                //$("#country-image").append(h1Tag);
+                $("#country-image").append(divTag);
+
             })
 
 
